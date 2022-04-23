561a3d23cf425add4d04d4ea1d2b5add47296f77
Dorothy Ewuah <dorothyewuah@gmail.com>
2022-04-23 01:02:18 +0000

    Day 3: commit with forms, reactive forms module and creating custom validators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective } from './favorite.directive';
-import { categoryListPipe } from './category-list.pipe';
+import { FavoriteDirective } from './custom-directive/favorite.directive';
+import { categoryListPipe } from './custom-pipe/category-list.pipe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MediaItemComponent } from './mw-media-item/media-item.component';
+import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
+import { MediaItemListComponent } from './media-item-list/media-item-list.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
     FavoriteDirective,
-    categoryListPipe
+    categoryListPipe,
+    MediaItemFormComponent
   ],
   bootstrap: [
     AppComponent
