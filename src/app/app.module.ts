501a94bb5a71b0221d90ecf97dd3651dd1e3f749
Dorothy Ewuah <dorothyewuah@gmail.com>
2022-04-24 23:45:12 +0000

    Day 4: Http Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { FavoriteDirective } from './custom-directive/favorite.directive';
-import { categoryListPipe } from './custom-pipe/category-list.pipe';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MediaItemComponent } from './mw-media-item/media-item.component';
-import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
-import { MediaItemListComponent } from './media-item-list/media-item-list.component';
-import { lookupListToken, lookupLists } from './services/providers';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule, HttpXhrBackend } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { FavoriteDirective } from "./custom-directive/favorite.directive";
+import { categoryListPipe } from "./custom-pipe/category-list.pipe";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MediaItemComponent } from "./mw-media-item/media-item.component";
+import { MediaItemFormComponent } from "./media-item-form/media-item-form.component";
+import { MediaItemListComponent } from "./media-item-list/media-item-list.component";
+import { lookupListToken, lookupLists } from "./services/providers";
+import { MockXHRBackend } from "./backend/mock-xhr-backend";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   declarations: [
     AppComponent,
     MediaItemComponent,
@@ -23,13 +21,16 @@ import { lookupListToken, lookupLists } from './services/providers';
     categoryListPipe,
     MediaItemFormComponent
   ],
-  bootstrap: [
-    AppComponent
-  ],
+  bootstrap: [AppComponent],
   providers: [
     {
-      provide: lookupListToken, useValue: lookupLists
-    }
-  ]
+      provide: lookupListToken,
+      useValue: lookupLists,
+    },
+    {
+      provide: HttpXhrBackend,
+      useClass: MockXHRBackend,
+    },
+  ],
 })
 export class AppModule {}
