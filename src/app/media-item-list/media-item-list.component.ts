75be00e6e5239bbd6b43a9f148e898b857185dad
Dorothy Ewuah <dorothyewuah@gmail.com>
2022-04-25 23:59:08 +0000

    Day 5: Http POST

--- src/app/media-item-list/media-item-list.component.ts
+++ src/app/media-item-list/media-item-list.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { MediaItemService } from '../services/media-item-service';
-
+import { MediaItem } from '../services/media-item-service';
 @Component({
   selector: 'app-media-item-list',
   templateUrl: './media-item-list.component.html',
   styleUrls: ['./media-item-list.component.css']
 })
 export class MediaItemListComponent implements OnInit {
-  mediaItems;
+  medium = '';
+  mediaItems: MediaItem[];
 
   constructor(private mediaItemService: MediaItemService) { }
 
   ngOnInit() {
-    this.mediaItems= this.mediaItemService.get();
+    this.mediaItemService.get(this.mediaItems)
+        .subscribe(mediaItems =>{
+          this.mediaItems = mediaItems;
+        });
   }
 
   onMediaItemDelete(mediaItem) {
     this.mediaItemService.delete(mediaItem);
    }
+
+   getMediaItems(medium: string) {
+    this.medium = medium;
+    this.mediaItemService.get(medium)
+      .subscribe(mediaItems => {
+        this.mediaItems = mediaItems;
+      });
+  }
 }
--- src/app/services/media-item-service.ts
+++ src/app/services/media-item-service.ts
@@ -1,70 +1,47 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root",
 })
-
 export class MediaItemService {
-  mediaItems = [
-    {
-      id: 1,
-      name: "Firebug",
-      medium: "Series",
-      category: "Science Fiction",
-      year: 2010,
-      watchedOn: 1457166565384,
-      isFavorite: false,
-    },
-    {
-      id: 2,
-      name: "The Small Tall",
-      medium: "Movies",
-      category: "SitComs",
-      year: 2010,
-      watchedOn: 1457166565384,
-      isFavorite: false,
-    },
-    {
-      id: 3,
-      name: "Oliver Twist",
-      medium: "Telenovella",
-      category: "Romance",
-      year: 2022,
-      watchedOn: 1457166565384,
-      isFavorite: true,
-    },
-    {
-      id: 4,
-      name: "You Again",
-      medium: "Series",
-      category: "SitComs",
-      year: 2010,
-      watchedOn: 1457166565384,
-      isFavorite: false,
-    },
-    {
-      id: 5,
-      name: "Handsome Stranger",
-      medium: "Movies",
-      category: "SitComs",
-      year: 2011,
-      watchedOn: 1457166565384,
-      isFavorite: true,
-    },
-  ];
+  constructor(private http: HttpClient) {}
 
-  get() {
-    return this.mediaItems;
+  get(medium) {
+    const getOptions = {
+      params: {medium}
+    }
+    return this.http.get<MediaItemsResponse>("mediaItems", getOptions).pipe(
+      map((response) => {
+        return response.mediaItems;
+      })
+    );
   }
 
   add(mediaItem) {
-    this.mediaItems.push(mediaItem);
+    // this.mediaItems.push(mediaItem);
+    return this.http.post('mediaitems', mediaItem)
   }
   delete(mediaItem) {
-    const index = this.mediaItems.indexOf(mediaItem);
-    if (index >=0){
-    this.mediaItems.splice(index, 1);
-        
-    }
+    // const index = this.mediaItems.indexOf(mediaItem);
+    // if (index >= 0) {
+    //   this.mediaItems.splice(index, 1);
+    // }
+    return this.http.delete(`mediaitems/${mediaItem.id}`);
   }
+}
+
+interface MediaItemsResponse {
+  mediaItems: MediaItem[];
+}
+
+export interface MediaItem {
+  id: number;
+  name: string;
+  medium: string;
+  category: string;
+  year: number;
+  watchedOn: number;
+  isFavorite: boolean;
 }
